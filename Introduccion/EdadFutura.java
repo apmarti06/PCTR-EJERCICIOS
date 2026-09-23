4207a1508e45ca29c7adc81796130eeacef3cce4
apmarti06 <apachecomarti@gmail.com>
2026-09-23 16:01:15 +0000

    Añadimos nuevo File

---
+++ Introduccion/EdadFutura.java
@@ -0,0 +1,11 @@
+public class EdadFutura {
+    public static void main(String[] args) {
+        int edadActual = 17;
+        int aniosAPasar = 5;
+
+        int edadFutura = edadActual + aniosAPasar;
+
+        System.out.println("Edad actual: " + edadActual);
+        System.out.println("Dentro de " + aniosAPasar + " años tendrás: " + edadFutura + " años");
+    }
+}

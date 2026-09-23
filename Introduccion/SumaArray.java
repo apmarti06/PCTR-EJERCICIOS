d1d40cf5bb0aba05b3cfb1c24c26a7ddfbc37d1d
apmarti06 <apachecomarti@gmail.com>
2026-09-23 17:45:20 +0200

    Add SumaArray class to calculate sum and average

--- Introduccion/SumaArray.java
+++ Introduccion/SumaArray.java
@@ -1,1 +1,14 @@
+public class SumaArray {
+    public static void main(String[] args) {
+        int[] numeros = {3, 7, 2, 9, 4, 1};
+        int suma = 0;
 
+        for (int n : numeros) {
+            suma += n;
+        }
+
+        System.out.println("Números: " + java.util.Arrays.toString(numeros));
+        System.out.println("Suma total: " + suma);
+        System.out.println("Media: " + (suma / (double) numeros.length));
+    }
+}
